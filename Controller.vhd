a9502ee4ee128678a1b5312d7919a73c53aadea8
vibhav011 <vibhav200601@gmail.com>
2021-05-21 03:36:37 +0530

    Added state transition

--- Controller.vhd
+++ Controller.vhd
@@ -28,8 +28,10 @@ begin
 
 process(clk,state)	
 	variable op_code :std_logic_vector(3 downto 0);
+	variable next_state: FSMState;
 	  
 begin
+	next_state := state;
   case state is
 		when Sres =>
 			 c_write <= '0';
@@ -41,12 +43,32 @@ begin
 			 t3_write <= '0';
 			 rf_write <= '0';
 			 memory_write <= '0';
+			 next_state := S0;
 ----------------------------------------------------------
 		when S0 =>
-			 m_add <= pc_out;
-			 ir_in <= m_data_out;
-			 ir_write <= '1';
-			 op_code := ir_out(15 downto 12);
+			m_add <= pc_out;
+			ir_in <= m_data_out;
+			ir_write <= '1';
+			op_code := ir_out(15 downto 12);
+			case op_code is
+				when "0000" =>
+					next_state := S00;
+				when "0001" =>
+					next_state := S00;
+				when "0010" =>
+					next_state := S00;
+				when "0011" =>
+					next_state := S00;
+				when "0100" =>
+					next_state := S00;
+				when "0101" =>
+					next_state := S00;
+				when "0110" =>
+					next_state := S00;
+				when "0111" =>
+					next_state := S00;
+				when others => null;
+			end case;
 ----------------------------------------------------------
 		when S00 =>
 				pc_write <= '1';
@@ -54,18 +76,44 @@ begin
 				alu_a <= pc_out;
 				alu_b <= "0000000000000001";
 				pc_in <= alu_c;
+
+				if (op_code = "0000" or op_code = "0010") then
+					if ( ((not ir_out(0)) and (not ir_out(1))) or (ir_out(0) and z_out) or (ir_out(1) and c_out) = '1' ) then
+						next_state := S1;
+					else
+						next_state := S0;
+					end if;
+				elsif (op_code = "0001") then
+					next_state := S4;
+				elsif (op_code = "0101" or op_code = "0100") then
+					next_state := S8;
+				elsif (op_code = "0011") then
+					next_state := S6;
+				elsif (op_code = "0110" or op_code = "0111") then
+					next_state := S11;
+				else
+					next_state := S0;
+				end if;
 				
 ----------------------------------------------------------
 		when S1 =>
-		      t1_write <= '1';
+				t1_write <= '1';
 				t2_write <= '1';
 				rf_a1 <= ir_out(11 downto 9);
 				rf_a2 <= ir_out(8 downto 6);
 				t1 <= rf_d1;
 				t2 <= rf_d2;
+
+				if (op_code = "0000" or op_code = "0010") then
+					next_state := S2;
+				elsif (op_code = "1100") then
+					next_state := S16;
+				else
+					next_state := S0;
+				end if;
 ----------------------------------------------
 		when S2 =>
-		      t3_write <= '1';
+				t3_write <= '1';
 				c_write <= '1';
 				z_write <= '1';
 				if(op_code="0010") then
@@ -78,11 +126,27 @@ begin
 				t3 <= alu_c;
 				c_in <= alu_carry;
 				z_in <= alu_zero;
+
+				case op_code is
+					when "0000" =>
+						next_state := S3;
+					when "0010" =>
+						next_state := S3;
+					when "0001" =>
+						next_state := S5;
+					when "0101" =>
+						next_state := S10;
+					when "0100" =>
+						next_state := S9;
+					when others =>
+						next_state := S0;
+				end case;
 ---------------------------------------------------------------			 
 		when S3 =>
 			   rf_write <= '1';
 				rf_d3 <= t3;
 				rf_a3 <= ir_out(5 downto 3);
+				next_state := S0;
 -----------------------------------------------------------------
 		when S4 =>
 				t1_write <= '1';
@@ -90,37 +154,59 @@ begin
 				rf_a1 <= ir_out(11 downto 9);
 				t1 <= rf_d1;
 				t2 <= "0000000000" & ir_out(5 downto 0);
+
+				if (op_code = "0001") then
+					next_state := S2;
+				else
+					next_state := S0;
+				end if;
 -----------------------------------------------------------------				
 		when S5 =>
 				rf_write <= '1';
 				rf_d3 <= t3;
 				rf_a3 <= ir_out(8 downto 6);
+				next_state := S0;
 -----------------------------------------------------------------				
 		when S6 =>
 			   t3_write <= '1';
 				alu_a <= "0000000" & ir_out(8 downto 0);
+
+				if (op_code = "0011") then
+					next_state := S7;
+				else
+					next_state := S0;
+				end if;
 -----------------------------------------------------------------
 		when S7 =>
 			   rf_write <= '1';
 				rf_d2 <= t3;
 				rf_a3 <= ir_out(11 downto 9);
+				next_state := S0;
 -----------------------------------------------------------------
       when S8 =>
 				rf_a2 <= ir_out(8 downto 6);
 				t2 <= rf_d2;
 				t1 <= "0000000000" & ir_out(5 downto 0);
+
+				if (op_code = "0100" or op_code = "0101") then
+					next_state := S2;
+				else
+					next_state := S0;
+				end if;
 -----------------------------------------------------------------
       when S9 =>
 				rf_write <= '1';
-			   m_add <= t3;
+				m_add <= t3;
 				rf_d3 <= m_data_out;
 				rf_a3 <= ir_out(11 downto 9);
+				next_state := S0;
 -----------------------------------------------------------------
       when S10 =>
 				memory_write <= '1';
-            m_add <= t3;
+            	m_add <= t3;
 				rf_a1 <= ir_out(11 downto 9);
 				m_data_in <= rf_d1;
+				next_state := S0;
 -----------------------------------------------------------------
 	   when S11 =>
 				t1_write <= '1';
@@ -128,12 +214,26 @@ begin
 				rf_a1 <= ir_out(11 downto 9);
 				t1 <= rf_d1;
 				t2 <= "000";
+
+				if (op_code = "0111") then
+					next_state := S15;
+				elsif (op_code = "0110") then
+					next_state := S12;
+				else
+					next_state := S0;
+				end if;
 -----------------------------------------------------------------
       when S12 =>
 				rf_write <= '1';
 				m_add <= t1;
 				rf_d3 <= m_data_out;
 				rf_a3 <= t2;
+
+				if (op_code = "0110") then
+					next_state := S13;
+				else
+					next_state := S0;
+				end if;
 -----------------------------------------------------------------
       when S13 =>
 				t1_write <= '1';
@@ -141,6 +241,12 @@ begin
 				alu_a <= t1;
 				alu_b <= "0000000000000001";
 				t1 <= alu_c;
+
+				if (op_code = "0110" or op_code = "0111") then
+					next_state := S14;
+				else
+					next_state := S0;
+				end if;
 -----------------------------------------------------------------
       when S14 =>
 				t2_write <= '1';
@@ -148,18 +254,42 @@ begin
 				alu_a <= t2;
 				alu_b <= "0000000000000001";
 				t2 <= alu_c;
+
+				if (t2(2 downto 0) = "000") then
+					next_state := S0;
+				else
+					if (op_code = "0111") then
+						next_state := S15;
+					elsif (op_code = "0110") then
+						next_state := S12;
+					else
+						next_state := S0;
+					end if;
+				end if;
 -----------------------------------------------------------------				  
       when S15 =>
 				memory_write <= '1';
 				rf_a1 <= t2;
 				m_data_in <= rf_d1;
 				m_add <= t1;
+
+				if (op_code = "0111") then
+					next_state := S13;
+				else
+					next_state := S0;
+				end if;
 -----------------------------------------------------------------
       when S16 =>
 				alu_op <= "01";
 				alu_a <= t1;
 				alu_b <= t2;
 				t3 <= alu_c;
+
+				if (t3(2 downto 0) = "000") then
+					next_state := S17;
+				else
+					next_state := S00;
+				end if;
 -----------------------------------------------------------------
       when S17 =>
 				pc_write <= '1';
@@ -167,11 +297,20 @@ begin
 				alu_a <= pc_out;
 				alu_b <= "0000000000" & ir_out(5 downto 0);
 				pc_in <= alu_c;
+				next_state := S0;
 -----------------------------------------------------------------
       when S18 =>
 				rf_write <= '1';
 				rf_a3 <= ir_out(11 downto 9);
 				rf_d3 <= pc_out;
+
+				if (op_code = "1000") then
+					next_state := S19;
+				elsif (op_code = "1001") then
+					next_state := S20;
+				else
+					next_state := S0;
+				end if;
 -----------------------------------------------------------------
       when S19 =>
 				pc_write <= '1';
@@ -179,11 +318,13 @@ begin
 				alu_a <= pc_out;
 				alu_b <= "0000000" & ir_out(8 downto 0);
 				pc_in <= alu_c;
+				next_state := S0;
 -----------------------------------------------------------------
       when S20 =>
 				pc_write <= '1';
 				rf_a1 <= ir_out(8 downto 6);
 				pc_in <= rf_d1;
+				next_state := S0;
 -----------------------------------------------------------------				
 		when others => null;
   end case;		
